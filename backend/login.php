16baed71e2978b469940c011c5782746046ad5ee
Vijay Pal Gautam <vijaypal.hestabit@gmail.com>
2022-07-12 19:13:39 +0530

    check error

--- backend/login.php
+++ backend/login.php
@@ -3,6 +3,12 @@
 error_reporting(E_ALL);
 ini_set("diplay_errors", 1);
 
+
+$username = ($_POST['user_name']);
+$password = ($_POST['password']);
+echo  $username;
+die;
+
 use validator\validator;
 
 include "validator.php";
@@ -13,7 +19,7 @@ class login
 {
     function __construct()
     {
-        
+
         $username = ($_POST['user_name']);
         $password = ($_POST['password']);
         // $email = null;
@@ -21,7 +27,7 @@ class login
         $this->login_validate($username, $password,);
     }
 
-    public function login_validate($username,$password)
+    public function login_validate($username, $password)
     {
         $check_user = new LoginController();
 
@@ -39,14 +45,14 @@ class login
         }
 
 
-        
+
         // login on database
-        
-        if($usernamer == 1 && $passwordr == 1){
-           $res = $check_user->login($username,$password);
-            echo json_encode(['login'=>$res]);
-        }else{
-            $response = ['username' => $usernamer,'password' => $passwordr];
+
+        if ($usernamer == 1 && $passwordr == 1) {
+            $res = $check_user->login($username, $password);
+            echo json_encode(['login' => $res]);
+        } else {
+            $response = ['username' => $usernamer, 'password' => $passwordr];
             echo json_encode($response);
         }
     }
