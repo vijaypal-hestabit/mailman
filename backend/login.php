8c157b3c8a2c59c89a891dc426404f71099f169d
Vijay Pal Gautam <vijaypal.hestabit@gmail.com>
2022-07-12 21:37:20 +0530

    finding error

--- backend/login.php
+++ backend/login.php
@@ -1,6 +1,7 @@
 <?php
 
-// use validator\validator;
+echo "ffffff";
+use validator\validator;
 include "validator.php";
 include "process.php";
 include "controller/loginController.php";
@@ -11,42 +12,39 @@ class login
     {
         
         $username = $_POST['user_name'];
-        $password = ($_POST['password']);
-        // $email = null;
-        echo 'slfj';
-        die;
-        $this->login_validate($username, $password,);
+        $password = $_POST['password'];
+
+        // $this->login_validate($username, $password,);
     }
 
-    public function login_validate($username, $password)
-    {
-        die;
-        $check_user = new LoginController();
+    // public function login_validate($username, $password)
+    // {
+    //     $check_user = new LoginController();
 
-        // user name validation
-        if (!validator::is_require($username)['value']) {
-            $usernamer = ['username_error' => "Please enter user name"];
-        } else {
-            $usernamer = 1;
-        }
+    //     // user name validation
+    //     if (!validator::is_require($username)['value']) {
+    //         $usernamer = ['username_error' => "Please enter user name"];
+    //     } else {
+    //         $usernamer = 1;
+    //     }
 
-        if (!validator::is_require($password)['value']) {
-            $passwordr = ['password_error' => "Please enter password"];
-        } else {
-            $passwordr = 1;
-        }
-
+    //     if (!validator::is_require($password)['value']) {
+    //         $passwordr = ['password_error' => "Please enter password"];
+    //     } else {
+    //         $passwordr = 1;
+    //     }
 
 
-        // login on database
 
-        if ($usernamer == 1 && $passwordr == 1) {
-            $res = $check_user->login($username, $password);
-            echo json_encode(['login' => $res]);
-        } else {
-            $response = ['username' => $usernamer, 'password' => $passwordr];
-            echo json_encode($response);
-        }
-    }
+    //     // login on database
+
+    //     if ($usernamer == 1 && $passwordr == 1) {
+    //         $res = $check_user->login($username, $password);
+    //         echo json_encode(['login' => $res]);
+    //     } else {
+    //         $response = ['username' => $usernamer, 'password' => $passwordr];
+    //         echo json_encode($response);
+    //     }
+    // }
 }
 $ob = new login();
