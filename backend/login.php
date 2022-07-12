0cd769dd05343e86c3d34a8644117df2cd910c45
Vijay Pal Gautam <vijaypal.hestabit@gmail.com>
2022-07-12 21:43:01 +0530

    finding error

--- backend/login.php
+++ backend/login.php
@@ -19,36 +19,36 @@ class login
 
     public function login_validate()
     {
-        echo " inn ";
+        // echo " inn ";
         $username = $_POST['user_name'];
         $password = $_POST['password'];
-        print_r($_POST);
-    //     $check_user = new LoginController();
+        // print_r($_POST);
+        $check_user = new LoginController();
 
-    //     // user name validation
-    //     if (!validator::is_require($username)['value']) {
-    //         $usernamer = ['username_error' => "Please enter user name"];
-    //     } else {
-    //         $usernamer = 1;
-    //     }
+        // user name validation
+        if (!validator::is_require($username)['value']) {
+            $usernamer = ['username_error' => "Please enter user name"];
+        } else {
+            $usernamer = 1;
+        }
 
-    //     if (!validator::is_require($password)['value']) {
-    //         $passwordr = ['password_error' => "Please enter password"];
-    //     } else {
-    //         $passwordr = 1;
-    //     }
+        if (!validator::is_require($password)['value']) {
+            $passwordr = ['password_error' => "Please enter password"];
+        } else {
+            $passwordr = 1;
+        }
 
 
 
-    //     // login on database
+        // login on database
 
-    //     if ($usernamer == 1 && $passwordr == 1) {
-    //         $res = $check_user->login($username, $password);
-    //         echo json_encode(['login' => $res]);
-    //     } else {
-    //         $response = ['username' => $usernamer, 'password' => $passwordr];
-    //         echo json_encode($response);
-    //     }
+        if ($usernamer == 1 && $passwordr == 1) {
+            $res = $check_user->login($username, $password);
+            echo json_encode(['login' => $res]);
+        } else {
+            $response = ['username' => $usernamer, 'password' => $passwordr];
+            echo json_encode($response);
+        }
     }
 }
 $ob = new login();
