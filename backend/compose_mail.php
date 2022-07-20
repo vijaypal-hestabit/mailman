f997e2c5f5cb0309ab767697347abd17b927a768
Vijay Pal Gautam <vijaypal.hestabit@gmail.com>
2022-07-20 21:05:31 +0530

    testing attachment

--- backend/compose_mail.php
+++ backend/compose_mail.php
@@ -63,7 +63,8 @@ class compose extends CommonFunctions
                 $inbox_id = "";
             }
             $this->save_message($message, $user_id, $inbox_id);
-        } else if (isset($_FILES['files'])) {                 // set files path in draft
+        } else if (isset($_FILES['files'])) {   
+            var_dump($_FILES['files']);             // set files path in draft
             if($_FILES['files']['sise'] == '20971520'){
                 echo 'true';
             }
